52965b5b79187a4fc393ff6f0ea84e3e976e6fb7
geekskai <geeks.kai@gmail.com>
2026-05-17 11:45:01 +0800

    fix: Update image tag in footer to comply with Next.js best practices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -45,7 +45,12 @@ export function Footer() {
         </div>
 
         <a href="https://sellwithboost.com" target="_blank" rel="noopener noreferrer nofollow">
-          <img src="https://sellwithboost.com/badge/listing.svg" alt="Listed on Sell With boost" style={{ height: '40px', width: 'auto' }} />
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://sellwithboost.com/badge/listing.svg"
+            alt="Listed on Sell With boost"
+            style={{ height: "40px", width: "auto" }}
+          />
         </a>
       </div>
     </footer>
